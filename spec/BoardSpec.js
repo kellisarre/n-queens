944cdf0aae14ffa9e78257c20e1a784579bf3d90
kellisarre <kelli.sarre@gmail.com>
2019-01-16 21:45:03 -0600

    Added a test for the lower left corner because it wasn't working on the BoardViewer webpage.

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -60,6 +60,13 @@ describe('Board', function() {
       [0, 0, 0, 0],
       [0, 0, 1, 0]
     ]);
+
+    verifyConflictTypes(['majorDiagonal', 'queens'], [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 0, 0]
+    ]);
   });
 
   describe('Board with minor diagonal conflicts', function() {
